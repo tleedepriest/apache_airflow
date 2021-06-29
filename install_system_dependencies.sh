84c78a572043b5b36f9a676787351d292f618be7
DePriest, Travis L <tdepries@ida.org>
2021-06-29 09:43:21 -0400

    added a script copied from the prerequisites section of installing apache airflow so that it makes this project a bit more reproducible

---
+++ install_system_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install -y --no-install-recommends \
+        freetds-bin \
+        krb5-user \
+        ldap-utils \
+        libffi6 \
+        libsasl2-2 \
+        libsasl2-modules \
+        libssl1.1 \
+        locales  \
+        lsb-release \
+        sasl2-bin \
+        sqlite3 \
+        unixodbc
